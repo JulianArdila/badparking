e0c7c778273b9d7d0dc0dc83934138b304ad1c4e
Hector Paloma <jdavidardilar@gmail.com>
2018-10-26 21:42:40 -0500

    Avance de Back End

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'badparking',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = 'C:\\Users\\CECA\\Documents\\badparking\\backend\\media'
+MEDIA_URL = '/media/'
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,6 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
+
+from badparking import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('inicio/',views.principal),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
